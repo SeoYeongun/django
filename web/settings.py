fb117fc9dca8a2827f7d418a2fd8b2fa97160260
SeoYeongun <voe697754@gmail.com>
2024-12-11 08:21:51 +0900

    portfolio

--- web/settings.py
+++ web/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+MOVIE_API_KEY = env('MOVIE_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)balvb8rk!unh2*(juj&f-ls!9u1zd09anzat%!t@1i!(he+s9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,8 +59,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'web.urls'
-
-MOVIE_API_KEY = 'b44fdec16917ebb572412d2fecb13768'
 
 TEMPLATES = [
     {
